21eb78be1b316ac4046cd564aac139938df6afa3
LuizinhoXT <luiz.farias.work@outlook.com>
2024-10-03 22:14:01 -0300

    update 1_2_comandos_DML.sql and add quety.sql

---
+++ PBL_1_Ano_Fase2_MNGroup/query.sql
@@ -0,0 +1,4 @@
+UPDATE mc_funcionario SET vl_salario = 17560.0 where cd_funcionario IN (1);
+select nm_funcionario, vl_salario FROM mc_funcionario WHERE cd_funcionario = 5;
+
+Select * from mc_funcionario
