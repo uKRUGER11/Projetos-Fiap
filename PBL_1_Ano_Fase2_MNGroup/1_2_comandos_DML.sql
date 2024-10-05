d2e5115838009a7dd4c4c775533313db72cfdfaf
LuizinhoXT <luiz.farias.work@outlook.com>
2024-10-05 01:09:38 -0300

    update query e 1_2_comandos_DML

--- PBL_1_Ano_Fase2_MNGroup/1_2_comandos_DML.sql
+++ PBL_1_Ano_Fase2_MNGroup/1_2_comandos_DML.sql
@@ -35,10 +35,10 @@ insert
         'F',
         'Mulher Cisgenero',
         'Gerente Expert I de Vendas',
-        14978.0,
+        18978.0,
         'reb.sou@mc.com.br',
         'A',
-        TO_DATE('17-04-2023', 'DD-MM-YYYY'),
+        TO_DATE('17-04-2021', 'DD-MM-YYYY'),
         null        
     );
     
@@ -64,13 +64,13 @@ insert
         1, -- fk de mc_funcionario
         'Gustavo Rodrigues da Silva',
         TO_DATE('25-12-2002', 'DD-MM-YYYY'),
-        'H', -- realizar update 
+        'M', 
         'Homem Cisgênero Não Binário',
         'Analista Sênior de Vendas e Operações ',
         9897.0,
         'gus.rod@mc.com.br',
         'A',
-        TO_DATE('29-09-2023', 'DD-MM-YYYY'),
+        TO_DATE('29-09-2021', 'DD-MM-YYYY'),
         null        
     );
     
@@ -96,13 +96,13 @@ insert
         1, -- fk de mc_funcionario
         'Sara Gonçalves Oliveira',
         TO_DATE('01-07-2003', 'DD-MM-YYYY'),
-        'M',
+        'F',
         'Mulher Cisgênero',
         'Analista Pleno de Vendas e Operações ',
-        6435.0,
+        7435.0,
         'sar.gon@mc.com.br',
         'A',
-        TO_DATE('25-01-2023', 'DD-MM-YYYY'),
+        TO_DATE('25-01-2022', 'DD-MM-YYYY'),
         null        
     );
     
@@ -126,15 +126,15 @@ insert
         4, --pk de mc_funcionario
         1, --fk em mc_depto
         1, -- fk de mc_funcionario
-        'Luana Zink Bizarria',
+        'Geovanna Zink Bizarria',
         TO_DATE('09-08-1998', 'DD-MM-YYYY'),
-        'M',
+        'F',
         'Mulher Cisgênero',
         'Analista Sênior de Vendas e Operações ',
         9897.0,
         'lua.zin@mc.com.br',
         'A',
-        TO_DATE('25-03-2024', 'DD-MM-YYYY'),
+        TO_DATE('17-09-2024', 'DD-MM-YYYY'),
         null        
     );
 
@@ -159,19 +159,455 @@ insert
         1, --fk em mc_depto
         1, -- fk de mc_funcionario
         'Alice Cristina Souza',
-        TO_DATE('01-06-2024', 'DD-MM-YYYY'),
-        'M',
+        TO_DATE('01-06-2001', 'DD-MM-YYYY'),
+        'F',
         'Mulher Cisgênero',
         'Analista Junior de Vendas e Operações ',
         4960.0,
-        'lua.zin@mc.com.br',
+        'ali.cri@mc.com.br',
         'A',
         TO_DATE('25-03-2024', 'DD-MM-YYYY'),
         null        
     );
     
+-- Inserindo funcionarios do financeiro
+
+insert
+    into mc_funcionario (
+        cd_funcionario,
+        cd_depto,
+        cd_gerente,
+        nm_funcionario,
+        dt_nascimento,
+        fl_sexo_biologico,
+        ds_genero,
+        ds_cargo,
+        vl_salario,
+        ds_email,
+        st_func,
+        dt_cadastramento,
+        dt_desligamento
+    )
+    values (
+        6, --pk de mc_funcionario
+        2, --fk em mc_depto
+        null, -- fk de mc_funcionario
+        'Rafael Madjer',
+        TO_DATE('20-03-2000', 'DD-MM-YYYY'),
+        'M',
+        'Homem Cisgênero',
+        'Gerente Expert de Finanças',
+        17560.0,
+        'raf.mad@mc.com.br',
+        'A',
+        TO_DATE('10-12-2021', 'DD-MM-YYYY'),
+        null        
+    );
+    
+insert
+    into mc_funcionario (
+        cd_funcionario,
+        cd_depto,
+        cd_gerente,
+        nm_funcionario,
+        dt_nascimento,
+        fl_sexo_biologico,
+        ds_genero,
+        ds_cargo,
+        vl_salario,
+        ds_email,
+        st_func,
+        dt_cadastramento,
+        dt_desligamento
+    )
+    values (
+        7, --pk de mc_funcionario
+        2, --fk em mc_depto
+        6, -- fk de mc_funcionario
+        'Guilherme Bessi',
+        TO_DATE('15-02-2000', 'DD-MM-YYYY'),
+        'M',
+        'Homem Cisgênero',
+        'Analista Sênior de Finanças',
+        9890.0,
+        'gui.bes@mc.com.br',
+        'A',
+        TO_DATE('30-3-2024', 'DD-MM-YYYY'),
+        null        
+    );    
+    
+insert
+    into mc_funcionario (
+        cd_funcionario,
+        cd_depto,
+        cd_gerente,
+        nm_funcionario,
+        dt_nascimento,
+        fl_sexo_biologico,
+        ds_genero,
+        ds_cargo,
+        vl_salario,
+        ds_email,
+        st_func,
+        dt_cadastramento,
+        dt_desligamento
+    )
+    values (
+        8, --pk de mc_funcionario
+        2, --fk em mc_depto
+        6, -- fk de mc_funcionario
+        'Geovanna Andreata Cruz',
+        TO_DATE('08-08-1999', 'DD-MM-YYYY'),
+        'M',
+        'Mulher Trans',
+        'Analista Pleno de Finanças',
+        8023.0,
+        'geo.and@mc.com.br',
+        'A',
+        TO_DATE('11-06-2024', 'DD-MM-YYYY'),
+        null        
+    ); 
+    
+insert
+    into mc_funcionario (
+        cd_funcionario,
+        cd_depto,
+        cd_gerente,
+        nm_funcionario,
+        dt_nascimento,
+        fl_sexo_biologico,
+        ds_genero,
+        ds_cargo,
+        vl_salario,
+        ds_email,
+        st_func,
+        dt_cadastramento,
+        dt_desligamento
+    )   
+    
+    values (
+    
+        9, --pk de mc_funcionario
+        2, --fk em mc_depto
+        6, -- fk de mc_funcionario
+        'Lucas Santos Silva',
+        TO_DATE('30-12-1999', 'DD-MM-YYYY'),
+        'F',
+        null,
+        'Analista junior de Finanças',
+        5032.0,
+        'luc.san@mc.com.br',
+        'A',
+        TO_DATE('28-01-2024', 'DD-MM-YYYY'),
+        null        
+    ); 
+
+-- reinserir a partir daqui Luiz
+
+-- Inserindo estados
+insert into mc_estado (sg_estado, nm_estado) values ('SP', 'São Paulo');
+
+insert into mc_estado (sg_estado, nm_estado) values ('MG', 'Minas Gerais');    
+
+insert into mc_estado (sg_estado, nm_estado) values ('SC', 'Santa Catarina');
+
+
+-- Inserindo duas cidades para cada estado
+
+insert into mc_cidade (cd_cidade, sg_estado, nm_cidade, cd_ibge, nr_ddd) values (1, 'SP', 'São Paulo', 3550308, 11);   
+insert into mc_cidade (cd_cidade, sg_estado, nm_cidade, cd_ibge, nr_ddd) values (2, 'SP', 'Francisco Morato', 3516309, 11);   
+
+insert into mc_cidade (cd_cidade, sg_estado, nm_cidade, cd_ibge, nr_ddd) values (3, 'MG', 'Belo Horizonte', 3106200, 31);   
+insert into mc_cidade (cd_cidade, sg_estado, nm_cidade, cd_ibge, nr_ddd) values (4, 'MG', 'Divinópolis', 3122306, 31);   
+
+insert into mc_cidade (cd_cidade, sg_estado, nm_cidade, cd_ibge, nr_ddd) values (5, 'SC', 'Palhoça', 4211900, 48);   
+insert into mc_cidade (cd_cidade, sg_estado, nm_cidade, cd_ibge, nr_ddd) values (6, 'SC', 'Florianópolis', 4205407, 48);   
+
+-- inseriondo 1 bairro para cada cidade
+
+
+insert into mc_bairro (cd_bairro, cd_cidade, nm_bairro, nm_zona_bairro) values (101, 1, 'Pinheiros', 'ZONA SUL');
+insert into mc_bairro (cd_bairro, cd_cidade, nm_bairro, nm_zona_bairro) values (102, 2, 'Recanto Regina', 'ZONA NNORTE');
+
+insert into mc_bairro (cd_bairro, cd_cidade, nm_bairro, nm_zona_bairro) values (103, 3, 'Serra Verde', 'ZONA NORTE');
+insert into mc_bairro (cd_bairro, cd_cidade, nm_bairro, nm_zona_bairro) values (104, 4, 'Francisco Machado Filho', null);
+
+insert into mc_bairro (cd_bairro, cd_cidade, nm_bairro, nm_zona_bairro) values (105, 5, 'Centro', null);
+insert into mc_bairro (cd_bairro, cd_cidade, nm_bairro, nm_zona_bairro) values (106, 6, 'Capoeiras', null);
+
+
+-- inserinto 2 logradouros para cada bairro
+
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101001,101, 'Rua dos Pinheiros - de 515 a 953 - lado ímpar', 05422011);
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101002,101, 'Avenida Rebouças - até 1278 - lado par', 05402000);
+
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101013,102, 'Avenida Santa Fé ', 07957020);
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101014,102, 'Rua San Diego', 07957060);
+
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101015,103, 'Alameda Industrial', 31630640);
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101016,103, 'Avenida Leontino Francisco Alves', 31630000);
+
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101027,104, 'Rua Minas Gerais - até 1717/1718', 35500007);
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101028,104, 'Rua Francisco Machado Filho', 35501047);
+
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101029,105, 'Avenida Barão do Rio Branco - lado ímpar', 88130101);
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101041,105, 'Avenida Prefeito Nelson Martins', 88131300);
+
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101078,106, 'Avenida Almirante Tamandaré, 480', 88080900);
+insert into mc_logradouro (cd_logradouro, cd_bairro, nm_logradouro, nr_cep) values (101079,106, 'Avenida Governador Ivo Silveira - de 801/802 a 1959/1960', 88085000);
+
+
+-- inserindo enderecos para 2 funcionarios
+insert
+    into mc_end_func (
+        cd_funcionario,
+        cd_logradouro,
+        nr_end,
+        ds_complemento_end,
+        dt_inicio,
+        dt_termino,
+        st_end
+    )
+    values (
+        1,
+        101013,
+        575,
+        'casa 2',
+        TO_DATE('09-01-2016', 'DD-MM-YYYY'),
+        null,
+        'A'
+    
+    );
+    
+insert
+    into mc_end_func (
+        cd_funcionario,
+        cd_logradouro,
+        nr_end,
+        ds_complemento_end,
+        dt_inicio,
+        dt_termino,
+        st_end
+    )
+    values (
+        1,
+        101001,
+        702,
+        'apt_a23',
+        TO_DATE('09-01-2012', 'DD-MM-YYYY'),
+        TO_DATE('24-12-2015', 'DD-MM-YYYY'),
+        'I'    
+    );
+
+insert
+    into mc_end_func (
+        cd_funcionario,
+        cd_logradouro,
+        nr_end,
+        ds_complemento_end,
+        dt_inicio,
+        dt_termino,
+        st_end
+    )
+    values (
+        5,
+        101027,
+        1500,
+        'próximo a igreja',
+        TO_DATE('09-01-2022', 'DD-MM-YYYY'),
+        null,
+        'A'    
+    );
+
+insert
+    into mc_end_func (
+        cd_funcionario,
+        cd_logradouro,
+        nr_end,
+        ds_complemento_end,
+        dt_inicio,
+        dt_termino,
+        st_end
+    )
+    values (
+        6,
+        101029,
+        45,
+        null,
+        TO_DATE('15-01-2024', 'DD-MM-YYYY'),
+        null,
+        'A'    
+    );
+    
+insert
+    into mc_end_func (
+        cd_funcionario,
+        cd_logradouro,
+        nr_end,
+        ds_complemento_end,
+        dt_inicio,
+        dt_termino,
+        st_end
+    )
+    values (
+        6,
+        101002,
+        1100,
+        'há dois quarteirões do MC Donalds',
+        TO_DATE('02-07-2022', 'DD-MM-YYYY'),
+        TO_DATE('14-01-2024', 'DD-MM-YYYY'),
+        'I'    
+    );
+
+insert
+    into mc_end_func (
+        cd_funcionario,
+        cd_logradouro,
+        nr_end,
+        ds_complemento_end,
+        dt_inicio,
+        dt_termino,
+        st_end
+    )
+    values (
+        9,
+        101078,
+        890,
+        'próxio a praça do bairro',
+        TO_DATE('02-07-2022', 'DD-MM-YYYY'),
+        null,
+        'A'    
+    );
+
+
+-- inserindo 2 clientes pessoas fisicas e cliente pessoa juridica
+
+-- Clientes
+insert 
+    into mc_cliente (
+        nr_cliente,
+        nm_cliente,
+        qt_estrelas,
+        vl_medio_compra,
+        st_cliente,
+        ds_email,
+        nr_telefone,
+        nm_login,
+        ds_senha
+    ) 
+    values (
+        2101,
+        'Luiz Henrique Ribeiro Dos Santos Farias',
+        9,
+        1200.0,
+        'A',
+        'luizfariashot@outlook.com',
+        '11-91234-5678',
+        'luizisxt092',
+        'senha123'
+    );
+
+insert 
+    into mc_cliente (
+        nr_cliente,
+        nm_cliente,
+        qt_estrelas,
+        vl_medio_compra,
+        st_cliente,
+        ds_email,
+        nr_telefone,
+        nm_login,
+        ds_senha
+    ) 
+    values (
+        2102,
+        'Victor Macedo',
+        4,
+        19000.23,
+        'I',
+        'victor_maceds@gmail.com',
+        '11-94321-5678',
+        'victosmad12',
+        '123senha'
+    );
+
+insert 
+    into mc_cliente (
+        nr_cliente,
+        nm_cliente,
+        qt_estrelas,
+        vl_medio_compra,
+        st_cliente,
+        ds_email,
+        nr_telefone,
+        nm_login,
+        ds_senha
+    ) 
+    values (
+        2103,
+        'Gabriel Kruguer',
+        0,
+        null,
+        'I',
+        'gabs.krug@outlook.com',
+        '11-92145-9978',
+        'kruger219038',
+        'sen123ha'
+    );
+
+-- Cliente pessoa fisica
+
+insert 
+    into mc_cli_fisica (
+       nr_cliente, 
+       dt_nascimento, 
+       fl_sexo_biologico, 
+       ds_genero, 
+       nr_cpf
+    )
+    values (
+        2101,
+        TO_DATE('03-03-2003', 'DD-MM-YYYY'),
+        'M',
+        null,
+        12312345627       
+    );
+ 
+insert 
+    into mc_cli_fisica (
+       nr_cliente, 
+       dt_nascimento, 
+       fl_sexo_biologico, 
+       ds_genero, 
+       nr_cpf
+    )
+    values (
+        2103,
+        TO_DATE('06-06-2005', 'DD-MM-YYYY'),
+        'M',
+        'Homem Cisgênero',
+        56789012345       
+    );
+
+-- Cliente pessoa jurídica
+
+insert 
+    into mc_cli_juridica (
+       nr_cliente, 
+       dt_fundacao, 
+       nr_cnpj, 
+       nr_inscr_est
+    )
+    values (
+        2102,
+        TO_DATE('13-12-2016', 'DD-MM-YYYY'),
+        01410663000147,
+        null 
+    );   
+    
+-- até a letra e
 COMMIT    
--- Inserindo funcionarios do comercial
+
 
 
 
--- PBL_1_Ano_Fase2_MNGroup/query.sql
+++ PBL_1_Ano_Fase2_MNGroup/query.sql
@@ -1,4 +1,43 @@
-UPDATE mc_funcionario SET vl_salario = 17560.0 where cd_funcionario IN (1);
-select nm_funcionario, vl_salario FROM mc_funcionario WHERE cd_funcionario = 5;
+UPDATE mc_funcionario SET cd_gerente = null;
+DELETE FROM mc_cliente ;
+COMMIT
+-- Commandos DQL
+-- Retorna todos os registros de uma tabela
+select 
+    * 
+from 
+    mc_cliente
 
-Select * from mc_funcionario
+-- Retorna o nome e valor do salario de um funcionário específico  
+select 
+    nm_funcionario, 
+    vl_salario 
+FROM 
+    mc_funcionario 
+WHERE 
+    cd_funcionario = 5;
+
+-- Query abaixo retorna todos os endereços cadastrados
+SELECT
+    l.nm_logradouro,
+    b.nm_bairro,
+    c.nm_cidade,
+    e.nm_estado
+FROM
+         mc_logradouro l
+    INNER JOIN mc_bairro b ON ( l.cd_bairro = b.cd_bairro )
+    INNER JOIN mc_cidade c ON ( b.cd_cidade = c.cd_cidade )
+    INNER JOIN mc_estado e ON ( c.sg_estado = e.sg_estado )
+
+-- Query abaixo retorna a quantidade de cada registro único nas tabelas logradouro, bairro, cidade, estado
+SELECT
+    COUNT(DISTINCT l.cd_logradouro) AS qty_lougradouro,
+    COUNT(DISTINCT b.cd_bairro)     AS qty_bairro,
+    COUNT(DISTINCT c.cd_cidade)     AS qty_cidade,
+    COUNT(DISTINCT e.sg_estado)     AS qty_estado
+FROM
+    mc_logradouro l
+    LEFT JOIN mc_bairro     b ON ( l.cd_bairro = b.cd_bairro )
+    LEFT JOIN mc_cidade     c ON ( b.cd_cidade = c.cd_cidade )
+    LEFT JOIN mc_estado     e ON ( c.sg_estado = e.sg_estado )
+
