c9845653edc7394d31a78f39e8ed9799b4233e45
LuizinhoXT <luiz.farias.work@outlook.com>
2024-10-27 17:35:13 -0300

    update 1_2_bloco_anonimo.sql

--- PBL_1_Ano_Fase3_MNGroup/1_2_bloco_anonimo.sql
+++ PBL_1_Ano_Fase3_MNGroup/1_2_bloco_anonimo.sql
@@ -2,6 +2,8 @@
 
 declare
     ocorrencia mc_sgv_ocorrencia_sac%rowtype;
+    v_DS_TIPO_CLASSIFICACAO_SAC varchar(30);
+    v_vl_unitario_lucro number (8,2);
     CURSOR cr_ocor IS
         select
             sc.nr_sac, sc.dt_abertura_sac, sc.hr_abertura_sac, sc.tp_sac,
@@ -20,7 +22,9 @@ declare
                                         inner join mc_estado es on (cd.sg_estado = es.sg_estado)) cli_es on (cli.nr_cliente = cli_es.nr_cliente);
                                  
 begin
-    FOR ocorrencia in cr_ocor LOOP
+    
+    for ocorrencia in cr_ocor loop
+        
         dbms_output.put_line('Número do SAC: ' ||  ocorrencia.nr_sac);
         dbms_output.put_line('Data de abertura do SAC: ' ||  ocorrencia.dt_abertura_sac);
         dbms_output.put_line('Hora de abertura do SAC: ' ||  ocorrencia.hr_abertura_sac);
@@ -33,8 +37,56 @@ begin
         dbms_output.put_line('Nome do Cliente associado ao SAC: ' ||  ocorrencia.nm_cliente);
         dbms_output.put_line('Sigla do estado relacionado ao cliente: ' ||  ocorrencia.sg_estado);
         dbms_output.put_line('Estado relacionado ao cliente: ' ||  ocorrencia.nm_estado);
-    END LOOP;
+        
+        EXIT WHEN cr_ocor%notfound;
+    -- Inserindo os dados na tabela mc_sgv_ocorrencia_sac
+        INSERT INTO mc_sgv_ocorrencia_sac (
+            nr_ocorrencia_sac, 
+            dt_abertura_sac, 
+            hr_abertura_sac, 
+            ds_tipo_classificacao_sac,
+            DS_INDICE_SATISFACAO_ATD_SAC, 
+            CD_PRODUTO, 
+            DS_PRODUTO, 
+            VL_UNITARIO_PRODUTO, 
+            VL_PERC_LUCRO,
+            VL_UNITARIO_LUCRO_PRODUTO, 
+            SG_ESTADO, 
+            NM_ESTADO, 
+            NR_CLIENTE, 
+            NM_CLIENTE,
+            VL_ICMS_PRODUTO
+        ) VALUES (
+            ocorrencia.nr_sac, 
+            ocorrencia.dt_abertura_sac, 
+            ocorrencia.hr_abertura_sac, 
+            'teste', 
+            null, 
+            ocorrencia.cd_produto, 
+            ocorrencia.ds_produto, 
+            ocorrencia.vl_unitario, 
+            v_vl_unitario_lucro, 
+            null, 
+            ocorrencia.sg_estado, 
+            ocorrencia.nm_estado,
+            ocorrencia.nr_cliente, 
+            ocorrencia.nm_cliente, 
+            null
+        );
+        END LOOP;
+
 end;
+
+
+Select * from mc_sgv_ocorrencia_sac;
+
+
+
+
+
+
+
+
 
 
 
